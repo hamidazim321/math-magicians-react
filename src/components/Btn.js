fe3db7affe781dd60b57daa4d450076a8622de2c
Hamid Azim <hamidazim321@gmail.com>
2023-08-09 08:56:50 -0700

    Add events

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function CalcBtn(props) {
-  const { text } = props;
+function CalcBtn({ text, handleClick }) {
   const buttonClass = text === '0' ? 'btn btn-zero' : 'btn';
   const red = ['+', '-', '=', '÷', 'x'].includes(text) ? 'btn-red' : '';
   return (
-    <button type="button" className={`${buttonClass} ${red}`}>{text}</button>
+    <button
+      type="button"
+      className={`${buttonClass} ${red}`}
+      onClick={(e) => {
+        handleClick(e.target.textContent);
+      }}
+    >
+      {text}
+    </button>
   );
 }
 CalcBtn.propTypes = {
   text: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default CalcBtn;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,44 @@
+import React, { useState } from 'react';
 import './Calculator.css';
 import CalcBtn from './Btn';
+import calculate from '../logic/Calculate';
 
 function Calculator() {
+  const [calcObj, updateCalcObj] = useState(
+    {
+      operation: null,
+      total: null,
+      next: null,
+    },
+  );
+
+  const handleClick = (btnName) => {
+    updateCalcObj(calculate(calcObj, btnName));
+  };
+
   return (
     <div className="calculator">
-      <div className="calculator-screen" />
-      <CalcBtn text="AC" />
-      <CalcBtn text="+/-" />
-      <CalcBtn text="%" />
-      <CalcBtn text="÷" />
-      <CalcBtn text="7" />
-      <CalcBtn text="8" />
-      <CalcBtn text="9" />
-      <CalcBtn text="x" />
-      <CalcBtn text="4" />
-      <CalcBtn text="5" />
-      <CalcBtn text="6" />
-      <CalcBtn text="-" />
-      <CalcBtn text="1" />
-      <CalcBtn text="2" />
-      <CalcBtn text="3" />
-      <CalcBtn text="+" />
-      <CalcBtn text="0" />
-      <CalcBtn text="=" />
+      <div className="calculator-screen">
+        {calcObj.total}
+      </div>
+      <CalcBtn text="AC" handleClick={handleClick} />
+      <CalcBtn text="+/-" handleClick={handleClick} />
+      <CalcBtn text="%" handleClick={handleClick} />
+      <CalcBtn text="÷" handleClick={handleClick} />
+      <CalcBtn text="7" handleClick={handleClick} />
+      <CalcBtn text="8" handleClick={handleClick} />
+      <CalcBtn text="9" handleClick={handleClick} />
+      <CalcBtn text="x" handleClick={handleClick} />
+      <CalcBtn text="4" handleClick={handleClick} />
+      <CalcBtn text="5" handleClick={handleClick} />
+      <CalcBtn text="6" handleClick={handleClick} />
+      <CalcBtn text="-" handleClick={handleClick} />
+      <CalcBtn text="1" handleClick={handleClick} />
+      <CalcBtn text="2" handleClick={handleClick} />
+      <CalcBtn text="3" handleClick={handleClick} />
+      <CalcBtn text="+" handleClick={handleClick} />
+      <CalcBtn text="0" handleClick={handleClick} />
+      <CalcBtn text="=" handleClick={handleClick} />
     </div>
   );
 }
