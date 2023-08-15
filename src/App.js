ad5362adde4fffe6f259dc0061c33585c1c80055
Hamid Azim <hamidazim321@gmail.com>
2023-08-15 05:12:43 -0700

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quotes from './components/quotes';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Quotes />} />
+        <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
       </Routes>
     </BrowserRouter>
   );
