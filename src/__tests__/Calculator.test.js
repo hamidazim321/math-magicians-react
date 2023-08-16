41079b6d66d3a1948ef87689bf3cf504e138e617
Hamid Azim <hamidazim321@gmail.com>
2023-08-16 08:37:38 -0700

    Add tests for all buttons of the calculator

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -18,9 +18,9 @@ describe('Integration tests for calculator component', () => {
   test('2 + 2 should display 4', async () => {
     render(<Calculator />);
 
-    const btnTwo = await screen.findByText(/2/);
+    const btnTwo = await screen.findByText('2');
     const btnPlus = await screen.findByText('+');
-    const btnEqual = await screen.findByText(/=/);
+    const btnEqual = await screen.findByText('=');
 
     await fireEvent.click(btnTwo);
     await fireEvent.click(btnPlus);
@@ -31,4 +31,104 @@ describe('Integration tests for calculator component', () => {
 
     expect(await calcScreen.textContent).toBe('4');
   });
+
+  test('clicking AC should clear the screen', async () => {
+    render(<Calculator />);
+    const btnTwo = await screen.findByText('2');
+    const btnPlus = await screen.findByText('+');
+    const btnClear = await screen.findByText('AC');
+    await fireEvent.click(btnTwo);
+    await fireEvent.click(btnPlus);
+    await fireEvent.click(btnTwo);
+    await fireEvent.click(btnClear);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("");
+  });
+
+  test('Adding a negative number and a positive', async () => {
+    render(<Calculator />);
+    const btnTwo = await screen.findByText('2');
+    const btnPlus = await screen.findByText('+');
+    const btnEqual = await screen.findByText('=');
+    const btnNegative = await screen.findByText('+/-')
+    await fireEvent.click(btnTwo);
+    await fireEvent.click(btnNegative)
+    await fireEvent.click(btnPlus);
+    await fireEvent.click(btnTwo);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("0");
+  });
+
+  test('9 % 2 = 1', async () => {
+    render(<Calculator />);
+    const btn1 = await screen.findByText('9');
+    const btn2 = await screen.findByText('2')
+    const btnMod = await screen.findByText('%');
+    const btnEqual = await screen.findByText('=');
+    await fireEvent.click(btn1);
+    await fireEvent.click(btnMod);
+    await fireEvent.click(btn2);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("1");
+  });
+
+  test('9 - 2 = 7', async () => {
+    render(<Calculator />);
+    const btn1 = await screen.findByText('9');
+    const btn2 = await screen.findByText('2')
+    const btnMinus = await screen.findByText('-');
+    const btnEqual = await screen.findByText('=');
+    await fireEvent.click(btn1);
+    await fireEvent.click(btnMinus);
+    await fireEvent.click(btn2);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("7");
+  });
+
+  test('9 x 2 = 18', async () => {
+    render(<Calculator />);
+    const btn1 = await screen.findByText('9');
+    const btn2 = await screen.findByText('2')
+    const btnMinus = await screen.findByText('x');
+    const btnEqual = await screen.findByText('=');
+    await fireEvent.click(btn1);
+    await fireEvent.click(btnMinus);
+    await fireEvent.click(btn2);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("18");
+  });
+
+  test('4 ÷ 2 = 2', async () => {
+    render(<Calculator />);
+    const btn1 = await screen.findByText('4');
+    const btn2 = await screen.findByText('2')
+    const btnMinus = await screen.findByText('÷');
+    const btnEqual = await screen.findByText('=');
+    await fireEvent.click(btn1);
+    await fireEvent.click(btnMinus);
+    await fireEvent.click(btn2);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("2");
+  });
+
+  test('4 ÷ 0 = Math Error', async () => {
+    render(<Calculator />);
+    const btn1 = await screen.findByText('4');
+    const btn2 = await screen.findByText('0')
+    const btnMinus = await screen.findByText('÷');
+    const btnEqual = await screen.findByText('=');
+    await fireEvent.click(btn1);
+    await fireEvent.click(btnMinus);
+    await fireEvent.click(btn2);
+    await fireEvent.click(btnEqual);
+    const calcScreen = document.querySelector('.calculator-screen p');
+    expect(await calcScreen.textContent).toBe("Math Error");
+  });
+
+
 });
