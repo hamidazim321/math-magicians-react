4d39b07be7c4b5fa6df5719e36332617b07f5eb0
Hamid Azim <hamidazim321@gmail.com>
2023-08-16 03:44:57 -0700

    Add snapshot test for Home.js

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,7 @@
+import {render, screen, cleanup} from '@testing-library/react'
+import Home from '../components/Home'
+
+test('should render', ()=> {
+  const { asFragment } = render(<Home />);
+  expect(asFragment()).toMatchSnapshot();
+})
