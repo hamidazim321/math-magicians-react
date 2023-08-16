d5cc7abef042868f0717e6a86e0db7ae7540b89e
Hamid Azim <hamidazim321@gmail.com>
2023-08-16 06:52:24 -0700

    Add integration test for clicking on nabars

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,12 @@
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent, cleanup} from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom'
 import App from '../App'
+
+afterEach(()=>{
+	cleanup()
+})
 
 describe('Integration test for App', ()=> {
     test('Renders App', ()=> {
@@ -14,4 +19,42 @@ describe('Integration test for App', ()=> {
     		.toJSON();
 			expect(tree).toMatchSnapshot()
     })
+
+		test('load the Quotes component by clickin on quotes in Navbar', async ()=>{
+			const response = [
+				{
+					quote: 'mocked quote',
+				},
+			];
+			global.fetch = jest.fn().mockResolvedValue({
+				json: jest.fn().mockResolvedValue(response),
+			});
+					
+			render(
+				<MemoryRouter>
+					<App/>
+				</MemoryRouter>
+			)
+
+			const quotes = await screen.findByText(/Quotes/)
+			fireEvent.click(quotes)
+
+			expect((await screen.findByText(/mocked/)).textContent).toBe('mocked quote')
+		})
+
+		test('load the calculator component by clickin on Calculator in Navbar', async ()=>{
+
+			render(
+				<MemoryRouter>
+					<App/>
+				</MemoryRouter>
+			)
+
+			const calculator = await screen.findByText(/Calculator/)
+			fireEvent.click(calculator)
+
+			const calc = document.getElementsByClassName('calculator')[0]
+			expect(calc).toBeInTheDocument()
+
+		})
 })
